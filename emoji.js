992edf4548fd670951b26a92c59d2441abd89986
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-03-12 02:48:38 -0700

    this is project for emojis in DOM ,HTML, CSS

---
+++ emoji.js
@@ -0,0 +1,11 @@
+// const changeface = document.getElementById('face');
+const emojis =["😊","🤣","❤","😍","😒","👌","😘","🤷‍♂️","🤦‍♂️","🤦‍♀️","🙌","💕","✌","🤞","😉","😜","🤷‍♀️","🎂","🎉","🌹","👏"];
+const face = document.getElementById('face');
+const btn = document.getElementById("button");
+
+btn.addEventListener ("click", () => {
+    
+   let round =Math.floor(Math.random() * emojis.length);
+   let result = emojis[round];
+   face.innerHTML = result;
+});
